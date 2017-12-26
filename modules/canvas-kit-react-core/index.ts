934f833f50d92164002aa1cb64b5bb9a6beb08cb
Joseph <joseph.le@workday.com>
2017-12-26 11:06:39 -0800

    feat(fonts): Add fonts

--- modules/canvas-kit-react-core/index.ts
+++ modules/canvas-kit-react-core/index.ts
@@ -1,12 +1,14 @@
 import colors from './lib/colors'
 import depth from './lib/depth'
 import spacing from './lib/spacing'
+import fonts from './lib/fonts'
 import type from './lib/type'
 
 const canvas = {
   colors,
   depth,
   spacing,
+  fonts,
   type,
 }
 
