9e32ec0d7c3351d351d07f712cf16f79415118fd
Alex Nicholls <alex.nicholls@workday.com>
2018-01-04 17:06:26 -0800

    chore(depth): Add types to depth

--- modules/canvas-kit-react-core/lib/depth.ts
+++ modules/canvas-kit-react-core/lib/depth.ts
@@ -1,35 +1,36 @@
 import colors from './colors'
 
-const constants = {
+const shadow: { [x: string]: string } = {
   shadowColorLight: 'rgba(82, 97, 115, 0.14)',
   shadowColor: 'rgba(82, 97, 115, 0.18)',
+}
+
+const sizing: { [x: string]: number } = {
   baseOffset: 2,
   baseBlurRadius: 4,
 }
 
-const depth = {
+const depth: { [x: string]: Object } = {
   inset: {
-    'box-shadow': `inset 0 0 ${constants.baseBlurRadius * 2}px ${constants.shadowColorLight}`,
+    'box-shadow': `inset 0 0 ${sizing.baseBlurRadius * 2}px ${shadow.shadowColorLight}`,
   },
   1: {
-    'box-shadow': `0 ${constants.baseOffset}px ${constants.baseBlurRadius}px ${
-      constants.shadowColorLight
-    }`,
+    'box-shadow': `0 ${sizing.baseOffset}px ${sizing.baseBlurRadius}px ${shadow.shadowColorLight}`,
   },
   2: {
-    'box-shadow': `0 ${constants.baseOffset * 2}px ${constants.baseBlurRadius * 2}px ${
-      constants.shadowColor
+    'box-shadow': `0 ${sizing.baseOffset * 2}px ${sizing.baseBlurRadius * 2}px ${
+      shadow.shadowColor
     }`,
   },
   3: {
-    'box-shadow': `0 ${constants.baseOffset * 4}px ${constants.baseBlurRadius * 4}px ${
-      constants.shadowColor
+    'box-shadow': `0 ${sizing.baseOffset * 4}px ${sizing.baseBlurRadius * 4}px ${
+      shadow.shadowColor
     }`,
   },
   4: {
     border: `1px solid ${colors.soap500}`,
-    'box-shadow': `0 ${constants.baseOffset * 4}px ${constants.baseBlurRadius * 4}px ${
-      constants.shadowColor
+    'box-shadow': `0 ${sizing.baseOffset * 4}px ${sizing.baseBlurRadius * 4}px ${
+      shadow.shadowColor
     }`,
   },
 }
