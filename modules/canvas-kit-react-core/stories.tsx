7b6ca35bb751012bc0e6ae64e981db12eb14a079
Joseph <joseph.le@workday.com>
2018-01-08 19:22:30 -0800

    feat(type): Add story

--- modules/canvas-kit-react-core/stories.tsx
+++ modules/canvas-kit-react-core/stories.tsx
@@ -33,3 +33,44 @@ storiesOf('Canvas Kit/Core', module)
       </div>
     )
   })
+  .add('Type', () => (
+    <div className="story">
+      <h1 className="section-label">Type</h1>
+      <h1 style={canvas.type.h1}>H1 Header</h1>
+      <h2 style={canvas.type.h2}>H2 Header</h2>
+      <h3 style={canvas.type.h3}>H3 Header</h3>
+      <h4 style={canvas.type.h4}>H4 Header</h4>
+      <h5 style={canvas.type.h5}>H5 Header</h5>
+      <p style={canvas.type.body}>
+        <strong>Body: </strong> Tacos chartreuse raclette single-origin coffee ethical tilde ennui.
+        Magna asymmetrical church-key farm-to-table dreamcatcher nisi iceland photo booth kitsch
+        next level pop-up banh mi quinoa exercitation hella. Raw denim organic enim laboris
+        sustainable. Polaroid occupy typewriter distillery. Kinfolk nisi man braid try-hard raw
+        denim, thundercats salvia intelligentsia jean shorts officia. Heirloom craft beer put a bird
+        on it occaecat
+      </p>
+      <p style={canvas.type.body2}>
+        <strong>Body 2: </strong> Tacos chartreuse raclette single-origin coffee ethical tilde
+        ennui. Magna asymmetrical church-key farm-to-table dreamcatcher nisi iceland photo booth
+        kitsch next level pop-up banh mi quinoa exercitation hella. Raw denim organic enim laboris
+        sustainable. Polaroid occupy typewriter distillery. Kinfolk nisi man braid try-hard raw
+        denim, thundercats salvia intelligentsia jean shorts officia. Heirloom craft beer put a bird
+        on it occaecat
+      </p>
+      {/* eslint-disable jsx-a11y/anchor-is-valid */}
+      <a href="#" style={canvas.type.link}>
+        Link Text
+      </a>{' '}
+      {/* eslint-enable jsx-a11y/anchor-is-valid */}
+      <br />
+      <span style={canvas.type.hint}>Hint Text</span>
+      <br />
+      {/* eslint-disable jsx-a11y/label-has-for */}
+      <label style={canvas.type.label1}>Label 1</label>
+      <br />
+      <label style={canvas.type.label2}>Label 2</label>
+      <br />
+      <label style={canvas.type.label3}>Label 3</label>
+      {/* eslint-enable jsx-a11y/label-has-for */}
+    </div>
+  ))
