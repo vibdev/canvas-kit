69d37c0c5353e5cdd20ff897c8b0a4714699d6a0
Joseph <joseph.le@workday.com>
2018-01-31 13:33:13 -0800

    feat(core): use canvas-depth-web

--- modules/canvas-kit-react-core/lib/depth.ts
+++ modules/canvas-kit-react-core/lib/depth.ts
@@ -1,3 +1,4 @@
+import canvasDepthWeb from '@workday/canvas-depth-web'
 import { CSSProperties } from './types'
 import colors from './colors'
 
@@ -9,33 +10,6 @@ export interface CanvasDepth {
   4: CSSProperties
 }
 
-const shadow: { [x: string]: string } = {
-  shadowColorLight: 'rgba(82, 97, 115, 0.14)',
-  shadowColor: 'rgba(82, 97, 115, 0.18)',
-}
-
-const sizing: { [x: string]: number } = {
-  baseOffset: 2,
-  baseBlurRadius: 4,
-}
-
-const depth: CanvasDepth = {
-  inset: {
-    boxShadow: `inset 0 0 ${sizing.baseBlurRadius * 2}px ${shadow.shadowColorLight}`,
-  },
-  1: {
-    boxShadow: `0 ${sizing.baseOffset}px ${sizing.baseBlurRadius}px ${shadow.shadowColorLight}`,
-  },
-  2: {
-    boxShadow: `0 ${sizing.baseOffset * 2}px ${sizing.baseBlurRadius * 2}px ${shadow.shadowColor}`,
-  },
-  3: {
-    boxShadow: `0 ${sizing.baseOffset * 4}px ${sizing.baseBlurRadius * 4}px ${shadow.shadowColor}`,
-  },
-  4: {
-    border: `1px solid ${colors.soap500}`,
-    boxShadow: `0 ${sizing.baseOffset * 4}px ${sizing.baseBlurRadius * 4}px ${shadow.shadowColor}`,
-  },
-}
+const depth: CanvasDepth = canvasDepthWeb
 
 export default depth
