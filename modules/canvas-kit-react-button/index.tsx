fbfe0901dc404b88a469de7a8e14a88668c7f569
Joseph <joseph.le@workday.com>
2018-03-14 14:16:35 -0700

    refactor(button): separate Button into lib folder

--- modules/canvas-kit-react-button/index.tsx
+++ modules/canvas-kit-react-button/index.tsx
@@ -1,6 +1,1 @@
-import React from 'react'
-import glamorous from 'glamorous'
-import canvas from '@workday/canvas-kit-react-core'
-import '@workday/canvas-kit-css-button/dist/canvas-kit-css-button.css'
-
-export default props => <button className="wdc-btn">{props.children}</button>
+export { default as Button } from './lib/Button'
---
+++ modules/canvas-kit-react-button/lib/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import glamorous from 'glamorous'
+import canvas from '@workday/canvas-kit-react-core'
+import '@workday/canvas-kit-css-button/dist/canvas-kit-css-button.css'
+
+export default props => <button className="wdc-btn">{props.children}</button>
