cafd56ea84015082906d6053eb9e0c31551f9e6e
Joseph <joseph.le@workday.com>
2018-05-03 16:11:58 -0700

    feat(fat): add "fixed" prop

--- modules/canvas-kit-react-fat/lib/FloatingActionToolbar.tsx
+++ modules/canvas-kit-react-fat/lib/FloatingActionToolbar.tsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
 import '@workday/canvas-kit-css-fat/dist/canvas-kit-css-fat.css'
 
-export default class FloatingActionToolbar extends Component<JSX.IntrinsicElements['div'], {}> {
+export interface FloatingActionToolbarProps {
+  fixed?: boolean
+}
+
+export default class FloatingActionToolbar extends Component<
+  JSX.IntrinsicElements['div'] & FloatingActionToolbarProps,
+  {}
+> {
   render() {
+    const classes = {
+      'wdc-fat': true,
+      'wdc-fat-fixed': this.props.fixed,
+    }
+
     return (
-      <div className="wdc-fat" {...this.props}>
+      <div {...this.props} className={classNames(classes, this.props.className)}>
         <div className="wdc-fat-container">{this.props.children}</div>
       </div>
     )
