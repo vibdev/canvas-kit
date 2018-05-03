1bec4cc06f9a8c35a6025c644fdb871f860ad2b1
Joseph <joseph.le@workday.com>
2018-05-03 16:00:34 -0700

    feat(fat): expose natural HTML props

--- modules/canvas-kit-react-fat/lib/FloatingActionToolbar.tsx
+++ modules/canvas-kit-react-fat/lib/FloatingActionToolbar.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import '@workday/canvas-kit-css-fat/dist/canvas-kit-css-fat.css'
 
-export default class FloatingActionToolbar extends Component<{}, {}> {
+export default class FloatingActionToolbar extends Component<JSX.IntrinsicElements['div'], {}> {
   render() {
     return (
-      <div className="wdc-fat">
+      <div className="wdc-fat" {...this.props}>
         <div className="wdc-fat-container">{this.props.children}</div>
       </div>
     )
