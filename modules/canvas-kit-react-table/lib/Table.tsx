13fab37e3958968bb97bebbba99e7adafd992576
Joseph Le <joseph.le@workday.com>
2018-05-14 15:33:36 -0700

    refactor(table): remove data and column functionality

--- modules/canvas-kit-react-table/lib/Table.tsx
+++ modules/canvas-kit-react-table/lib/Table.tsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import classNames from 'classnames'
+import '@workday/canvas-kit-css-table/dist/canvas-kit-css-table.css'
 
-export default class Table extends Component<{}, {}> {
+export default class Table extends Component<JSX.IntrinsicElements['table'], {}> {
   public render() {
-    return <div>Table</div>
+    return (
+      <table {...this.props} className={classNames('wdc-table', this.props.className)}>
+        {this.props.children}
+      </table>
+    )
   }
 }
--- modules/canvas-kit-react-table/stories.tsx
+++ modules/canvas-kit-react-table/stories.tsx
@@ -8,9 +8,25 @@ import README from './README.md'
 
 storiesOf('Canvas Kit/Table', module)
   .addDecorator(withReadme(README))
-  .add('All', () => (
-    <div className="story">
-      <h1 className="section-label">Table</h1>
-      <Table />
-    </div>
-  ))
+  .add('All', () => {
+    const columns = ['ID', 'Name', 'Position', 'Location']
+    const data = [
+      [1, 'Aidan Brown', 'Product Manager', 'San Francisco, CA'],
+      [2, 'Betty Chen', 'Product Designer', 'San Francisco, CA'],
+      [3, 'Helen Gonzalez', 'Application Developer', 'Portland, OR'],
+      [4, 'Timothy May', 'VP, Product Development', 'San Francisco, CA'],
+    ]
+    return (
+      <div className="story">
+        <h1 className="section-label">Table</h1>
+        <Table>
+          <thead>
+            <tr>{columns.map((col, i) => <th key={i}>{col}</th>)}</tr>
+          </thead>
+          <tbody>
+            {data.map((row, i) => <tr key={i}>{row.map((c, i) => <td key={i}>{c}</td>)}</tr>)}
+          </tbody>
+        </Table>
+      </div>
+    )
+  })
