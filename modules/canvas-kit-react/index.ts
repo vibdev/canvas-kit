4c0a6bfd3d6e1f28ccef0bcbc291c4a06d6ecc30
Joseph <joseph.le@workday.com>
2018-05-29 13:34:55 -0700

    feat(icon): add icon to canvas-kit-react exports

--- modules/canvas-kit-react/index.ts
+++ modules/canvas-kit-react/index.ts
@@ -2,4 +2,5 @@ import canvas from '@workday/canvas-kit-react-core'
 
 export { canvas }
 export * from '@workday/canvas-kit-react-button'
+export * from '@workday/canvas-kit-react-icon'
 export * from '@workday/canvas-kit-react-table'
