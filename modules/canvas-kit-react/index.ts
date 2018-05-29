e26adfa18ed38db910f99049b88b5a8c361c299f
Joseph <joseph.le@workday.com>
2018-05-29 10:59:53 -0700

    feat(table): add table to canvas-kit-react exports

--- modules/canvas-kit-react/index.ts
+++ modules/canvas-kit-react/index.ts
@@ -1,5 +1,7 @@
 import canvas from '@workday/canvas-kit-react-core'
 import Button from '@workday/canvas-kit-react-button'
+import Table from '@workday/canvas-kit-react-table'
 
 export { canvas }
 export { Button }
+export { Table }
