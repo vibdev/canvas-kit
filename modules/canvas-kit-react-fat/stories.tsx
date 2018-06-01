6ffd361340de9fba991f92a50a924436db08b002
Joseph <joseph.le@workday.com>
2018-06-01 10:30:08 -0700

    docs(fat): separate fixed into its own story

--- modules/canvas-kit-react-fat/stories.tsx
+++ modules/canvas-kit-react-fat/stories.tsx
@@ -9,7 +9,7 @@ import README from './README.md'
 
 storiesOf('Canvas Kit/Floating Action Toolbar', module)
   .addDecorator(withReadme(README))
-  .add('All', () => (
+  .add('Default', () => (
     <div className="story">
       <h1 className="section-label">Floating Action Toolbar</h1>
       <FloatingActionToolbar>
@@ -17,7 +17,11 @@ storiesOf('Canvas Kit/Floating Action Toolbar', module)
         <Button>Button</Button>
         <Button>Button</Button>
       </FloatingActionToolbar>
-
+    </div>
+  ))
+  .add('Fixed', () => (
+    <div className="story">
+      <h1 className="section-label">Floating Action Toolbar - Fixed</h1>
       <FloatingActionToolbar fixed={true}>
         <Button buttonType={ButtonTypes.Primary}>Fixed</Button>
         <Button>Action</Button>
