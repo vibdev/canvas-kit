f5d084bde51ab9c9e8c5799dc4930054dfeb31d0
Joseph Le <joseph.le@workday.com>
2018-09-27 13:10:16 -0700

    refactor(CookieBanner): remove state from component

--- modules/canvas-kit-react-cookie-banner/lib/CookieBanner.tsx
+++ modules/canvas-kit-react-cookie-banner/lib/CookieBanner.tsx
@@ -3,28 +3,11 @@ import styled from 'react-emotion';
 import {commonColors, type, spacing} from '@workday/canvas-kit-react-core';
 import Button from '@workday/canvas-kit-react-button';
 
-const Banner = styled('div')(type.body, {
-  backgroundColor: commonColors.background,
-  display: 'flex',
-  padding: spacing.m,
-  alignItems: 'center',
-  justifyContent: 'center',
-});
+interface BannerProps {
+  isClosed?: boolean;
+}
 
-const BannerItem = styled('div')({
-  paddingLeft: spacing.s,
-  paddingRight: spacing.s,
-});
-
-const CookieSettingsLink = styled('a')({
-  ...type.link,
-  fontWeight: 500,
-  whiteSpace: 'nowrap',
-});
-
-const Link = styled('a')(type.link);
-
-interface Props {
+interface Props extends BannerProps {
   onAccept?: ((e: React.MouseEvent<HTMLButtonElement>) => void);
 }
 
@@ -32,31 +15,35 @@ interface State {
   acceptedCookies: boolean;
 }
 
+const Banner = styled('div')(
+  type.body,
+  {
+    backgroundColor: commonColors.background,
+    display: 'flex',
+    padding: spacing.m,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ({isClosed}: BannerProps) => (isClosed ? {visibility: 'hidden'} : null)
+);
+
+const BannerItem = styled('div')({
+  paddingLeft: spacing.s,
+  paddingRight: spacing.s,
+});
+
+const CookieSettingsLink = styled('a')(type.link, {
+  fontWeight: 500,
+  whiteSpace: 'nowrap',
+});
+
+const Link = styled('a')(type.link);
+
 export default class CookieBanner extends React.Component<Props, State> {
-  public constructor(props: Props) {
-    super(props);
-    this.state = {
-      acceptedCookies: false,
-    };
-  }
-
-  private onAccept = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    this.setState({
-      acceptedCookies: true,
-    });
-
-    if (this.props.onAccept) {
-      this.props.onAccept(event);
-    }
-  };
-
   public render() {
-    if (this.state.acceptedCookies) {
-      return null;
-    }
-
+    const {isClosed, onAccept} = this.props;
     return (
-      <Banner>
+      <Banner isClosed={isClosed}>
         <BannerItem>
           We use cookies to ensure that we give you the best experience on our website. If you
           continue without changing your settings, we’ll assume that you are willing to receive
@@ -66,7 +53,7 @@ export default class CookieBanner extends React.Component<Props, State> {
           <CookieSettingsLink href="#">Cookie Settings</CookieSettingsLink>
         </BannerItem>
         <BannerItem>
-          <Button onClick={this.onAccept}>Continue</Button>
+          <Button onClick={onAccept}>Continue</Button>
         </BannerItem>
       </Banner>
     );
--- modules/canvas-kit-react-cookie-banner/stories.tsx
+++ modules/canvas-kit-react-cookie-banner/stories.tsx
@@ -3,17 +3,59 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import {action} from '@storybook/addon-actions';
+import styled from 'react-emotion';
+import {Button} from '@workday/canvas-kit-react-button';
 
 import CookieBanner from './index'; // tslint:disable-line:import-name
 import README from './README.md';
+
+interface State {
+  acceptedCookies: boolean;
+}
+
+class BannerContainer extends React.Component<{}, State> {
+  public constructor(props: {}) {
+    super(props);
+    this.state = {
+      acceptedCookies: false,
+    };
+  }
+
+  private onAccept = (): void => {
+    action('accept-cookies');
+    this.setState({
+      acceptedCookies: true,
+    });
+  };
+
+  private reset = (): void => {
+    this.setState({
+      acceptedCookies: false,
+    });
+  };
+
+  public render() {
+    const Container = styled('div')({
+      backgroundColor: '#eff0f2',
+      padding: 24,
+      '& > *': {
+        marginBottom: 24,
+      },
+    });
+    return (
+      <Container>
+        <Button onClick={this.reset}>Reset Banner</Button>
+        <CookieBanner onAccept={this.onAccept} isClosed={this.state.acceptedCookies} />
+      </Container>
+    );
+  }
+}
 
 storiesOf('Canvas Kit/Cookie Banner', module)
   .addDecorator(withReadme(README))
   .add('All', () => (
     <div className="story">
       <h1 className="section-label">Cookie Banner</h1>
-      <div style={{backgroundColor: '#eff0f2', padding: 24}}>
-        <CookieBanner onAccept={action('accept-cookies')} />
-      </div>
+      <BannerContainer />
     </div>
   ));
