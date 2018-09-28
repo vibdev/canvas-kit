32ace83405ae2e181b6622b8b198af1532ae8571
Joseph Le <joseph.le@workday.com>
2018-09-28 11:30:13 -0700

    refactor(CookieBanner): use right instead of calc width

--- modules/canvas-kit-react-cookie-banner/lib/CookieBanner.tsx
+++ modules/canvas-kit-react-cookie-banner/lib/CookieBanner.tsx
@@ -39,7 +39,7 @@ const Banner = styled('div')(
     position: 'fixed',
     bottom: 0,
     left: 0,
-    width: `calc(100% - ${spacing.m} - ${spacing.m})`,
+    right: 0,
     '@media (max-width: 450px)': {
       flexDirection: 'column',
       alignItems: 'stretch',
