776c3b0f8f1454b2d6208335e7bd6386ee820fcb
Joseph Le <joseph.le@workday.com>
2018-10-03 13:47:49 -0700

    feat(emotion): convert styles to emotion

--- modules/canvas-kit-react-fat/README.md
+++ modules/canvas-kit-react-fat/README.md
@@ -4,18 +4,15 @@ Full width toolbar fixed to bottom of screen.
 
 ## Usage
 
-**This package uses CSS.**  
-If you're using webpack, you must enable a CSS loader.
-
-Although not required, [buttons](../canvas-kit-react-button) are often used in floating action
-toolbars.
-
 ### Floating Action Toolbar
 
 The toolbar can assume any `<div>` props.
 
-For details on styling, refer to
-[canvas-kit-css-fat](https://ghe.megaleo.com/design/canvas-kit-css/tree/master/modules/canvas-kit-css-fat).
+Although not required, [buttons](../canvas-kit-react-button) are often used in floating action
+toolbars.
+
+> The primary action button should be left aligned followed by secondary buttons. The primary button
+> is on the right only in task orchestration.
 
 ```jsx
 import { FloatingActionToolbar } from '@workday/canvas-kit-react-fat'
@@ -36,3 +33,15 @@ Add the `fixed` property to fix the toolbar to the bottom of the container.
   <Button>Button</Button>
 </FloatingActionToolbar>
 ```
+
+#### Responsive
+
+At 575px, responsive styles will take effect:
+
+- Applies a flex box to the buttons
+- Makes single-button groups full width
+- All buttons will become the same width (`flex: 1`).
+- Button order will become reversed, making left-aligned primary buttons right-aligned.
+
+> When on a mobile form factor, the button placement should flip to have the primary button on the
+> far right.
--- modules/canvas-kit-react-fat/stories.tsx
+++ modules/canvas-kit-react-fat/stories.tsx
@@ -9,23 +9,40 @@ import README from './README.md';
 
 storiesOf('Canvas Kit/Floating Action Toolbar', module)
   .addDecorator(withReadme(README))
-  .add('Default', () => (
+  .add('Two Button', () => (
     <div className="story">
       <h1 className="section-label">Floating Action Toolbar</h1>
       <FloatingActionToolbar>
         <Button buttonType={ButtonTypes.Primary}>Button</Button>
         <Button>Button</Button>
+      </FloatingActionToolbar>
+    </div>
+  ))
+  .add('Three Button', () => (
+    <div className="story">
+      <h1 className="section-label">Floating Action Toolbar</h1>
+      <FloatingActionToolbar>
+        <Button buttonType={ButtonTypes.Primary}>Button</Button>
         <Button>Button</Button>
       </FloatingActionToolbar>
     </div>
   ))
-  .add('Fixed', () => (
+  .add('Two Button - Fixed', () => (
     <div className="story">
       <h1 className="section-label">Floating Action Toolbar - Fixed</h1>
       <FloatingActionToolbar fixed={true}>
-        <Button buttonType={ButtonTypes.Primary}>Fixed</Button>
-        <Button>Action</Button>
-        <Button>Toolbar</Button>
+        <Button buttonType={ButtonTypes.Primary}>Button</Button>
+        <Button>Button</Button>
+      </FloatingActionToolbar>
+    </div>
+  ))
+  .add('Three Button - Fixed', () => (
+    <div className="story">
+      <h1 className="section-label">Floating Action Toolbar - Fixed</h1>
+      <FloatingActionToolbar fixed={true}>
+        <Button buttonType={ButtonTypes.Primary}>Button</Button>
+        <Button>Button</Button>
+        <Button>Button</Button>
       </FloatingActionToolbar>
     </div>
   ));
