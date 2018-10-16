3bfbf999e84f32318060b041dceba5ab280c96a4
Lynn Chyi <lynn.chyi@workday.com>
2018-10-16 11:14:18 -0700

    refactor(header): clean up render method and component comp

--- modules/canvas-kit-react-header/lib/Header.tsx
+++ modules/canvas-kit-react-header/lib/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {cx, css} from 'emotion';
+import {css} from 'emotion';
 import styled from 'react-emotion';
 import {type, spacing} from '@workday/canvas-kit-react-core';
 import {DubLogoTitle, WorkdayLogoTitle} from './parts';
@@ -25,20 +25,123 @@ export interface HeaderProps {
 
 const HeaderShell = styled('div')<HeaderProps>(
   {
-    label: 'header-style',
     ...verticalCenterStyle,
     boxSizing: 'border-box',
     ...type.body,
     WebkitFontSmoothing: 'antialiased',
     MozOsxFontSmoothing: 'grayscale',
   },
-  (props: HeaderProps) => ({
-    height: props.variant === HeaderVariant.Dub ? HeaderHeight.Small : HeaderHeight.Large,
-    background: themes[props.themeColor].background,
-    ...themes[props.themeColor].depth,
-    color: themes[props.themeColor].color,
+  ({variant, themeColor}) => ({
+    height: variant === HeaderVariant.Dub ? HeaderHeight.Small : HeaderHeight.Large,
+    background: themes[themeColor].background,
+    ...themes[themeColor].depth,
+    color: themes[themeColor].color,
   })
 );
+
+const BrandSlot = styled('div')(
+  {
+    height: '100%',
+  },
+  (props: {centered?: boolean}) => ({
+    flexGrow: props.centered ? 'unset' : 1,
+  })
+);
+
+const BrandLink = styled('a')({
+  ...type.link,
+
+  '&:hover': {
+    textDecoration: 'none',
+  },
+
+  '&:focus': {
+    textDecoration: 'none',
+  },
+});
+
+const navStyle = css({
+  nav: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    height: 'inherit',
+
+    '& ul': {
+      ...verticalCenterStyle,
+      justifyContent: 'center',
+      listStyleType: 'none',
+      padding: 0,
+      margin: 0,
+      height: 'inherit',
+
+      '& li': {
+        ...verticalCenterStyle,
+        margin: `0 ${spacing.s}`,
+        fontSize: '14px',
+        fontWeight: 700,
+        height: 'inherit',
+      },
+      '& li:first-child': {
+        marginLeft: 0,
+      },
+      '& li:last-child': {
+        marginRight: 0,
+      },
+
+      '& li a': {
+        boxSizing: 'border-box',
+        ...verticalCenterStyle,
+        color: 'inherit',
+        textDecoration: 'none',
+        height: 'inherit',
+        padding: `0px ${spacing.xxs}`,
+      },
+    },
+  },
+});
+
+const ChildrenSlot = styled('div')<HeaderProps>(
+  {
+    ...verticalCenterStyle,
+    justifyContent: 'flex-end',
+    height: '100%',
+
+    '> *': {
+      margin: `0 ${spacing.s}`,
+    },
+  },
+  ({centeredNav = false, variant}) => ({
+    flexGrow: centeredNav ? 1 : 'unset',
+
+    '> *:last-child': {
+      marginRight: variant === HeaderVariant.Dub ? spacing.s : spacing.l,
+    },
+  }),
+  navStyle
+);
+
+class Brand extends React.Component<HeaderProps> {
+  render() {
+    return (
+      <span>
+        {this.props.variant === HeaderVariant.Dub &&
+          (this.props.brand || (
+            <DubLogoTitle
+              title={this.props.title ? this.props.title : ''}
+              themeColor={this.props.themeColor}
+            />
+          ))}
+        {this.props.variant === HeaderVariant.Full &&
+          (this.props.brand || (
+            <WorkdayLogoTitle
+              title={this.props.title ? this.props.title : ''}
+              themeColor={this.props.themeColor}
+            />
+          ))}
+      </span>
+    );
+  }
+}
 
 export class Header extends React.Component<HeaderProps> {
   static Theme = HeaderTheme;
@@ -62,17 +165,16 @@ export class Header extends React.Component<HeaderProps> {
    */
   private renderChildren(children: React.ReactNode): React.ReactNode {
     return React.Children.map(children, child => {
-      // We can take advantage of isValidElement's return type here, the rest of
-      // the callback will infer child as a ReactElement if this check fails
       if (!React.isValidElement(child)) {
         return child;
       }
 
+      // child is now guaranteed to be a valid ReactElement from the check above
+
       type Props = {children: React.ReactNode};
       const propsChildren = (child as React.ReactElement<Props>).props.children;
-      const hasChildren = React.Children.count(propsChildren) ? true : false;
 
-      if (hasChildren) {
+      if (React.Children.count(propsChildren)) {
         return React.cloneElement(child as React.ReactElement<Props>, {
           children: this.renderChildren(propsChildren),
         });
@@ -89,115 +191,19 @@ export class Header extends React.Component<HeaderProps> {
     });
   }
 
-  private defaultBrand(): React.ReactNode {
-    return (
-      <span>
-        {this.props.variant === HeaderVariant.Dub &&
-          (this.props.brand || (
-            <DubLogoTitle
-              title={this.props.title ? this.props.title : ''}
-              themeColor={this.props.themeColor}
-            />
-          ))}
-        {this.props.variant === HeaderVariant.Full &&
-          (this.props.brand || (
-            <WorkdayLogoTitle
-              title={this.props.title ? this.props.title : ''}
-              themeColor={this.props.themeColor}
-            />
-          ))}
-      </span>
-    );
-  }
-
   render() {
-    const headerPadding = this.props.variant === HeaderVariant.Dub ? spacing.s : spacing.l;
-
-    const brandSlot = css({
-      label: 'brand-slot-style',
-      height: '100%',
-      flexGrow: this.props.centeredNav ? 'unset' : 1,
-    });
-
-    const childrenSlot = css({
-      label: 'children-slot-style',
-      ...verticalCenterStyle,
-      flexGrow: this.props.centeredNav ? 1 : 'unset',
-      justifyContent: 'flex-end',
-      height: '100%',
-      '> *': {
-        margin: `0 ${spacing.s}`,
-      },
-      '> *:last-child': {
-        marginRight: headerPadding,
-      },
-    });
-
-    const brandAnchor = css({
-      label: 'brand-anchor-style',
-      ...type.link,
-
-      '&:hover': {
-        textDecoration: 'none',
-      },
-
-      '&:focus': {
-        textDecoration: 'none',
-      },
-    });
-
-    const navStyle = css({
-      nav: {
-        flexGrow: 1,
-        justifyContent: 'center',
-        height: 'inherit',
-
-        '& ul': {
-          ...verticalCenterStyle,
-          justifyContent: 'center',
-          listStyleType: 'none',
-          padding: 0,
-          margin: 0,
-          height: 'inherit',
-
-          '& li': {
-            ...verticalCenterStyle,
-            margin: `0 ${spacing.s}`,
-            fontSize: '14px',
-            fontWeight: 700,
-            height: 'inherit',
-          },
-          '& li:first-child': {
-            marginLeft: 0,
-          },
-          '& li:last-child': {
-            marginRight: 0,
-          },
-
-          '& li a': {
-            boxSizing: 'border-box',
-            ...verticalCenterStyle,
-            color: 'inherit',
-            textDecoration: 'none',
-            height: 'inherit',
-            padding: `0px ${spacing.xxs}`,
-          },
-        },
-      },
-    });
-
     return (
       <HeaderShell themeColor={this.props.themeColor} variant={this.props.variant}>
-        <div className={brandSlot}>
+        <BrandSlot centered={this.props.centeredNav}>
           {this.props.brandUrl ? (
-            <a className={brandAnchor} href={this.props.brandUrl}>
-              {this.defaultBrand()}
-            </a>
+            <BrandLink href={this.props.brandUrl}>
+              <Brand {...this.props} />
+            </BrandLink>
           ) : (
-            this.defaultBrand()
+            <Brand {...this.props} />
           )}
-        </div>
-        <div className={cx(childrenSlot, navStyle)}>{this.renderChildren(this.props.children)}</div>
+        </BrandSlot>
+        <ChildrenSlot {...this.props}>{this.renderChildren(this.props.children)}</ChildrenSlot>
       </HeaderShell>
     );
   }
