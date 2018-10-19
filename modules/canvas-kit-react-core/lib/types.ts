e55f8798b2715db5e4c757b2f9a8d66d7483dfcb
Lynn Chyi <lynn.chyi@workday.com>
2018-10-19 14:15:36 -0700

    chore(deps): deprecate glamorous

--- modules/canvas-kit-react-core/lib/types.ts
+++ modules/canvas-kit-react-core/lib/types.ts
@@ -1,5 +1,4 @@
 import {CSSProperties as RCP} from 'react';
-import {CSSProperties as GCP} from 'glamorous';
 import {CSSObject} from 'create-emotion';
 
-export type CSSProperties = RCP & GCP & CSSObject;
+export type CSSProperties = RCP & CSSObject;
