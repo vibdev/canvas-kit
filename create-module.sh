80fdf9b4a06647fdbb52f358ad4f703ae8562e12
Alex Nicholls <anicholls3@gmail.com>
2018-10-23 13:55:59 -0700

    chore(packagejson): Update types location in all package json files

--- create-module.sh
+++ create-module.sh
@@ -49,7 +49,7 @@ cat > $packageJson << EOF
   "main": "dist/commonjs/index.js",
   "module": "dist/es6/index.js",
   "sideEffects": false,
-  "types": "dist/types/index.d.ts",
+  "types": "dist/es6/index.d.ts",
   "repository": {
     "type": "git",
     "url": "https://ghe.megaleo.com/design/canvas-kit-react/tree/master/modules/canvas-kit-react-$name"
