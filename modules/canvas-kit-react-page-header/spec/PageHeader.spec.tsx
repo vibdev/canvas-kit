11eef894dd211edfbbf27e9b017e68471d05f84b
Lynn Chyi <lynn.chyi@workday.com>
2018-11-06 16:31:18 -0800

    test(page-header): add tests

--- modules/canvas-kit-react-page-header/spec/PageHeader.spec.tsx
+++ modules/canvas-kit-react-page-header/spec/PageHeader.spec.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import PageHeader from '../lib/PageHeader';
 import {mount} from 'enzyme';
+import {createMatchers} from 'jest-emotion';
+import * as emotion from 'emotion';
 import {SystemIcon} from '@workday/canvas-kit-react-icon';
 import {exportIcon, fullscreenIcon} from '@workday/canvas-system-icons-web';
 import {colors} from '@workday/canvas-kit-react-core';
+
+expect.extend(createMatchers(emotion));
 
 describe('Page Header', () => {
   const cb = jest.fn();
@@ -17,7 +21,12 @@ describe('Page Header', () => {
     expect(component.find('h2').contains(testTitle));
   });
 
-  test('should alter SystemIcon components', () => {
+  test('should have a maxWidth in marketing contexts', () => {
+    const component = mount(<PageHeader title="Marketing Context" marketing={true} />);
+    expect(component.find('div').first()).toHaveStyleRule('max-width', '1440px');
+  });
+
+  test('should set correct SystemIcon color and colorHover props', () => {
     const testColors = {
       color: colors.blackberry100,
       colorHover: colors.cinnamon200,
@@ -49,5 +58,31 @@ describe('Page Header', () => {
         .filter('.icon1')
         .props().colorHover
     ).toBe(colors.blueberry200);
+  });
+  test('should render a copy of the children', () => {
+    const component = mount(
+      <PageHeader title="With Children">
+        <a href="#">
+          <SystemIcon className="icon1" icon={exportIcon} />
+        </a>
+        <a href="#">
+          <SystemIcon className="icon2" icon={fullscreenIcon} />
+        </a>
+      </PageHeader>
+    );
+
+    // Using @ts-ignore because we're 1) invoking a private method on a class and 2) passing stuff in we shouldn't
+    // @ts-ignore
+    expect(component.instance().renderChildren('string')).toEqual(['string']); // tests !isValidElement branch
+    // @ts-ignore
+    expect(component.instance().renderChildren(null)).toEqual(null); // tests what happens when no children exist
+    // @ts-ignore
+    expect(
+      component.instance().renderChildren(<SystemIcon className="icon2" icon={fullscreenIcon} />)
+    ).toHaveLength(1); // tests handling of SystemIcons
+    // @ts-ignore
+    expect(component.instance().renderChildren(<PageHeader title="Page Header" />)[0].type).toBe(
+      PageHeader
+    ); // this should render what was passed in, not an <a> tag
   });
 });
