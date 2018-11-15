2696ffc3da6910283fc47a635a37003c5144c608
chad sahlhoff <chad.sahlhoff@workday.com>
2018-11-14 16:01:03 -0800

    refactor(ButtonStyles): added ButtonStyles.ts for style objects

--- modules/canvas-kit-react-button/lib/ButtonBase.tsx
+++ modules/canvas-kit-react-button/lib/ButtonBase.tsx
@@ -7,142 +7,11 @@ import {ButtonProps} from './Button';
 import {ButtonHTMLAttributes, ClassAttributes, HTMLAttributes} from 'react';
 // @ts-ignore: To appease the TS god
 import {StyledOtherComponent} from 'create-emotion-styled';
+import * as ButtonStyles from './ButtonStyles';
 
 export const BUTTON_HEIGHT_LARGE: number = 40;
 export const BUTTON_HEIGHT_MEDIUM: number = 24;
 export const BUTTON_HEIGHT_SMALL: number = 18;
-
-// TODO convert these to generic styles
-const highlightStyles = {
-  background: canvas.colors.soap200,
-  border: canvas.colors.soap200,
-  text: canvas.colors.blueberry500,
-  activeBackground: canvas.colors.soap500,
-  activeBorder: canvas.colors.soap500,
-  activeText: canvas.colors.blueberry500,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: canvas.colors.soap100,
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.soap200,
-  focusBorder: canvas.colors.blueberry500,
-  focusText: canvas.colors.blueberry500,
-  hoverBackground: canvas.colors.soap400,
-  hoverBorder: canvas.colors.soap400,
-  hoverText: canvas.colors.blueberry500,
-};
-
-const outlineBlueStyles = {
-  background: 'transparent',
-  border: canvas.colors.blueberry400,
-  text: canvas.colors.blueberry400,
-  activeBackground: canvas.colors.blueberry500,
-  activeBorder: canvas.colors.blueberry500,
-  activeText: canvas.colors.frenchVanilla100,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: canvas.colors.soap500,
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.blueberry400,
-  focusBorder: null,
-  focusText: canvas.colors.frenchVanilla100,
-  hoverBackground: canvas.colors.blueberry400,
-  hoverBorder: canvas.colors.blueberry400,
-  hoverText: canvas.colors.frenchVanilla100,
-};
-
-const outlineDarkStyles = {
-  background: 'transparent',
-  border: canvas.colors.soap500,
-  text: canvas.colors.blackPepper400,
-  activeBackground: canvas.colors.licorice600,
-  activeBorder: null,
-  activeText: canvas.colors.frenchVanilla100,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: canvas.colors.soap500,
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.licorice500,
-  focusBorder: canvas.colors.blueberry500,
-  focusText: canvas.colors.frenchVanilla100,
-  hoverBackground: canvas.colors.licorice500,
-  hoverBorder: null,
-  hoverText: canvas.colors.frenchVanilla100,
-};
-
-const outlineWhiteStyles = {
-  background: 'transparent',
-  border: canvas.colors.frenchVanilla100,
-  text: canvas.colors.frenchVanilla100,
-  activeBackground: canvas.colors.soap200,
-  activeBorder: canvas.colors.frenchVanilla100,
-  activeText: canvas.colors.blackPepper400,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: canvas.colors.soap500,
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.frenchVanilla100,
-  focusBorder: canvas.colors.blueberry500,
-  focusText: canvas.colors.blackPepper400,
-  hoverBackground: canvas.colors.frenchVanilla100,
-  hoverBorder: null,
-  hoverText: canvas.colors.blackPepper400,
-};
-
-const textStyles = {
-  background: 'transparent',
-  border: 'transparent',
-  text: canvas.colors.blueberry400,
-  activeBackground: 'transparent',
-  activeBorder: 'transparent',
-  activeText: canvas.colors.blueberry500,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: canvas.colors.soap500,
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.blueberry100,
-  focusBorder: null,
-  focusText: canvas.colors.blueberry400,
-  hoverBackground: 'transparent',
-  hoverBorder: 'transparent',
-  hoverText: canvas.colors.blueberry500,
-};
-
-const textDarkStyles = {
-  background: 'transparent',
-  border: 'transparent',
-  text: canvas.colors.frenchVanilla100,
-  activeBackground: 'transparent',
-  activeBorder: 'transparent',
-  activeText: canvas.colors.frenchVanilla100,
-  disabledBackground: canvas.colors.frenchVanilla100,
-  disabledBorder: 'transparent',
-  disabledText: canvas.colors.licorice300,
-  focusBackground: canvas.colors.frenchVanilla100,
-  focusBorder: null,
-  focusText: canvas.colors.licorice500,
-  hoverBackground: 'transparent',
-  hoverBorder: 'transparent',
-  hoverText: canvas.colors.frenchVanilla100,
-};
-
-const canvasBase = {
-  boxSizing: 'border-box',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  fontSize: '13px',
-  borderRadius: '999px',
-  border: '1px solid transparent',
-  boxShadow: 'none',
-  position: 'relative',
-  cursor: 'pointer',
-  outline: 'none',
-  transition: 'all 120ms ease-in',
-  '&:hover:active': {transitionDuration: '40ms'}, // Makes the "down" state of the button happens faster than the hover state, so it animates in correctly.
-  '&:disabled, &:disabled:active': {cursor: 'default', boxShadow: 'none'},
-};
-
-const textBase = {
-  ...canvasBase,
-  borderRadius: '3px;',
-  '&:hover': {textDecoration: 'underline'},
-};
 
 export const ButtonBaseCon = styled('button')<ButtonProps>(
   // TODO: Support data-whatinput='input'
@@ -223,19 +92,7 @@ export const ButtonBaseCon = styled('button')<ButtonProps>(
 );
 
 export const ButtonBaseLabel = styled('span')<ButtonProps>(
-  {
-    position: 'relative', // Fixes an IE issue with text within button moving on click
-    ':hover:active': {
-      backgroundColor: 'transparent',
-    },
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    fontWeight: 500,
-    fontFamily: '"Roboto", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
-    WebkitFontSmoothing: 'antialiased',
-    MozOsxFontSmoothing: 'grayscale',
-  },
+  ButtonStyles.labelBaseStyles,
   ({buttonSize}) => {
     if (buttonSize === ButtonSizes.Medium) {
       return {
@@ -254,21 +111,7 @@ export const ButtonBaseLabel = styled('span')<ButtonProps>(
 );
 
 export const ButtonDataLabel = styled('span')<ButtonProps>(({buttonType}) => {
-  return {
-    position: 'relative',
-    ':hover:active': {
-      backgroundColor: 'transparent',
-    },
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    fontWeight: 200,
-    fontFamily: '"Roboto", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
-    WebkitFontSmoothing: 'antialiased',
-    MozOsxFontSmoothing: 'grayscale',
-    paddingLeft: '12px',
-    fontSize: '10px',
-  };
+  return ButtonStyles.dataLabelBaseStyles;
 });
 
 function getButtonBase(buttonType: ButtonTypes) {
@@ -281,10 +124,10 @@ function getButtonBase(buttonType: ButtonTypes) {
     case ButtonTypes.OutlineDark:
     case ButtonTypes.OutlineWhite:
     default:
-      return canvasBase;
+      return ButtonStyles.canvasBaseStyles;
     case ButtonTypes.Text:
     case ButtonTypes.TextDark:
-      return textBase;
+      return ButtonStyles.textBaseStyles;
   }
 }
 
@@ -298,16 +141,16 @@ function getButtonColors(buttonType: ButtonTypes) {
     case ButtonTypes.Delete:
       return canvas.buttonColors.delete;
     case ButtonTypes.Highlight:
-      return highlightStyles;
+      return ButtonStyles.highlightColors;
     case ButtonTypes.OutlineBlue:
-      return outlineBlueStyles;
+      return ButtonStyles.outlineBlueColors;
     case ButtonTypes.OutlineDark:
-      return outlineDarkStyles;
+      return ButtonStyles.outlineDarkColors;
     case ButtonTypes.OutlineWhite:
-      return outlineWhiteStyles;
+      return ButtonStyles.outlineWhiteColors;
     case ButtonTypes.Text:
-      return textStyles;
+      return ButtonStyles.textColors;
     case ButtonTypes.TextDark:
-      return textDarkStyles;
+      return ButtonStyles.textDarkColors;
   }
 }
