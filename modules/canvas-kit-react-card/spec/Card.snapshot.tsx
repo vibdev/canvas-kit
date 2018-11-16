cf26886ec01d7fc2fdfe1439dc04f3c9de41ff02
Joseph Le <joseph.le@workday.com>
2018-11-16 12:15:29 -0500

    test(Card): add snapshots

---
+++ modules/canvas-kit-react-card/spec/Card.snapshot.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Card, {CardSize} from '../lib/Card';
+import * as renderer from 'react-test-renderer';
+
+describe('Card Snapshots', () => {
+  test('renders as expected', () => {
+    const component = renderer.create(<Card>Card</Card>);
+    expect(component).toMatchSnapshot();
+  });
+
+  test('renders a card with title', () => {
+    const component = renderer.create(<Card title="Card Title">Card</Card>);
+    expect(component).toMatchSnapshot();
+  });
+
+  const cards = Array.from(Array(12).keys()).map(size => (
+    <Card title="Title" size={(size + 1) as CardSize} key={size} />
+  ));
+
+  cards.forEach((card, i) => {
+    test(`renders a card with size ${i + 1}`, () => {
+      const component = renderer.create(card);
+      expect(component).toMatchSnapshot();
+    });
+  });
+});
