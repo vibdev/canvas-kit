96ff62959cae41bdd5c243994c3f961b052cdd39
chad sahlhoff <chad.sahlhoff@workday.com>
2018-11-27 11:28:58 -0800

    feat(focus-ring): added arguments innerShadowColor and outerShadowColor

--- modules/canvas-kit-react-common/lib/styles/focusRing.ts
+++ modules/canvas-kit-react-common/lib/styles/focusRing.ts
@@ -7,11 +7,13 @@ import canvas from '@workday/canvas-kit-react-core';
  * By default, this mixin will create a 1px focus ring tightly wrapped
  * to the widget (no whitespace).
  *
- * @param ringWidth       Allows the user to specify the thickness in px of the focus ring.
- * @param separationWidth Allows the user to define the width in px of the whitespace
- *                        that there should be between the widget and the focus ring.
- * @param animate         Set property to false to opt out of the standard grow out of the middle animation
- * @param inset           Determines whether or not the focus ring is inset
+ * @param ringWidth        Allows the user to specify the thickness in px of the focus ring.
+ * @param separationWidth  Allows the user to define the width in px of the whitespace
+ *                         that there should be between the widget and the focus ring.
+ * @param animate          Set property to false to opt out of the standard grow out of the middle animation
+ * @param inset            Determines whether or not the focus ring is inset
+ * @param innerShadowColor Allows the user to specify the inner shadow color
+ * @param outerShadowColor Allows the user to specify the outer shadow color
  *
  * @returns {CSSObject} the css object for the focus ring style
  */
@@ -19,16 +21,14 @@ export default function focusRing(
   ringWidth: number = 2,
   separationWidth: number = 0,
   animate: boolean = true,
-  inset: boolean = false
+  inset: boolean = false,
+  innerShadowColor: string = canvas.colors.frenchVanilla100,
+  outerShadowColor: string = canvas.commonColors.focusOutline
 ): CSSObject {
   const endingInnerShadow =
-    (inset ? 'inset ' : '') + '0 0 0 ' + separationWidth + 'px ' + canvas.colors.frenchVanilla100;
+    (inset ? 'inset ' : '') + '0 0 0 ' + separationWidth + 'px ' + innerShadowColor;
   const endingOuterShadow =
-    (inset ? 'inset ' : '') +
-    '0 0 0 ' +
-    (ringWidth + separationWidth) +
-    'px ' +
-    canvas.commonColors.focusOutline;
+    (inset ? 'inset ' : '') + '0 0 0 ' + (ringWidth + separationWidth) + 'px ' + outerShadowColor;
   const endingBoxShadow = inset
     ? `${endingOuterShadow}`
     : `${endingInnerShadow}, ${endingOuterShadow}`;
