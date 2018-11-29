84975d185f0ac7aa0fbefb27576d52ee12a99c8f
Lynn Chyi <lynn.chyi@workday.com>
2018-11-28 16:50:17 -0800

    docs(storybook): hack together tsx syntax highlighting

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,7 @@
 import 'storybook-readme/register';
+// TODO: A semi-shoddy hack to enable syntax highlighting for tsx in storybook-readme.
+// Probably should open a PR to put tsx in storybook-readme instead?
+import 'prismjs/components/prism-tsx';
+
 import '@storybook/addon-actions/register';
 import '@storybook/addon-viewport/register';
