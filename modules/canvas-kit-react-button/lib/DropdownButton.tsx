7fca9d50fee8f9bcfe04f9c567d1473efb5a7c11
chad sahlhoff <chad.sahlhoff@workday.com>
2019-01-16 10:27:12 -0800

    feat(ude-buttons): dropdown
    cleaned up dropdown btn

--- modules/canvas-kit-react-button/lib/DropdownButton.tsx
+++ modules/canvas-kit-react-button/lib/DropdownButton.tsx
@@ -6,15 +6,15 @@ import {ButtonProps} from './Button';
 import {dropdownButtonStyles} from './ButtonStyles';
 import {caretDownIcon} from '@workday/canvas-system-icons-web';
 
+const DropdownButtonCon = styled('button')<ButtonProps>(
+  dropdownButtonStyles.styles,
+  ({buttonType}) => getButtonStyle(dropdownButtonStyles, buttonType!),
+  ({buttonSize}) => getButtonSize(dropdownButtonStyles, buttonSize!)
+);
+
 export default class DropdownButton extends React.Component<ButtonProps> {
   public render() {
     const {children} = this.props;
-
-    const DropdownButtonCon = styled('button')<ButtonProps>(
-      dropdownButtonStyles.styles,
-      ({buttonType}) => getButtonStyle(dropdownButtonStyles, buttonType!),
-      ({buttonSize}) => getButtonSize(dropdownButtonStyles, buttonSize!)
-    );
 
     return (
       <DropdownButtonCon {...this.props}>
