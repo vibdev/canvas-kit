b3eb60f8dc3f222d8d9e5d5cc37f7d8f7544c558
Alex Kong <alex.kong@workday.com>
2019-01-17 15:53:18 -0800

    fix(loading-animation): update name of stories to Loading Animation

--- modules/canvas-kit-react-loading-animation/stories.tsx
+++ modules/canvas-kit-react-loading-animation/stories.tsx
@@ -6,11 +6,11 @@ import withReadme from 'storybook-readme/with-readme';
 import LoadingAnimation from './index'; // tslint:disable-line:import-name
 import README from './README.md';
 
-storiesOf('Canvas Kit/Loading-animation', module)
+storiesOf('Canvas Kit/Loading Animation', module)
   .addDecorator(withReadme(README))
   .add('All', () => (
     <div className="story">
-      <h1 className="section-label">Loading-animation</h1>
+      <h1 className="section-label">Loading Animation</h1>
       <LoadingAnimation />
     </div>
   ));
