7e2a8b4a1540e51766a25ed7b7db23c403aa81c4
Lynn Chyi <lynn.chyi@workday.com>
2019-01-23 10:07:57 -0800

    test(header): add coverage to renderChildren in Header

--- modules/canvas-kit-react-header/spec/Header.spec.tsx
+++ modules/canvas-kit-react-header/spec/Header.spec.tsx
@@ -1,9 +1,30 @@
 import * as React from 'react';
 import Header from '../lib/Header';
 import {shallow, mount} from 'enzyme';
+import {SystemIcon} from '@workday/canvas-kit-react-icon';
+import {activityStreamIcon, justifyIcon} from '@workday/canvas-system-icons-web';
+import {themes} from '../lib/shared/themes';
+
+declare global {
+  interface Window {
+    resizeBy: (x: number, y: number) => void;
+  }
+}
+
+window.resizeBy = (x: number, y: number) => {
+  // @ts-ignore
+  window.innerWidth = x;
+  // @ts-ignore
+  window.innerHeight = y;
+  window.dispatchEvent(new Event('resize'));
+};
 
 describe('Header', () => {
   const cb = jest.fn();
+  beforeEach(() => {
+    window.resizeBy(1280, 1024);
+  });
+
   afterEach(() => {
     cb.mockReset();
   });
@@ -19,9 +40,10 @@ describe('Header', () => {
       md: 25,
       lg: 35,
     };
-    const component = shallow(<Header />);
-    // @ts-ignore
+    const component = shallow<Header>(<Header />);
     expect(component.instance().getScreenSize(widths.sm, breakpoints)).toBe('sm');
+    expect(component.instance().getScreenSize(widths.md, breakpoints)).toBe('md');
+    expect(component.instance().getScreenSize(widths.lg, breakpoints)).toBe('lg');
   });
 
   test('Searching something should call callback', () => {
@@ -30,5 +52,66 @@ describe('Header', () => {
     component.find('form').simulate('submit');
     expect(cb.mock.calls.length).toBe(1);
     component.unmount();
+  });
+
+  describe('How Header children render', () => {
+    const icon = <SystemIcon icon={activityStreamIcon} />;
+
+    beforeEach(() => {
+      window.resizeBy(1280, 1024);
+    });
+
+    test('Renders non-React child elements as is', () => {
+      const text = 'not a react element';
+      const wrapper = shallow<Header>(<Header>{text}</Header>);
+
+      expect(wrapper.contains(text));
+    });
+
+    test('Renders a div element as is', () => {
+      const wrapper = shallow<Header>(
+        <Header>
+          <div>Test</div>
+        </Header>
+      );
+      expect(
+        wrapper
+          .find('div')
+          .first()
+          .contains('Test')
+      ).toBeTruthy();
+    });
+
+    test('Automatically alters child SystemIcon color and colorHover', () => {
+      const theme = Header.Theme.Blue;
+
+      const expectedColors = {
+        color: themes[theme].systemIcon.color,
+        colorHover: themes[theme].systemIcon.colorHover,
+      };
+
+      const wrapper = mount<Header>(
+        <Header themeColor={theme}>
+          <a href="#">{icon}</a>
+        </Header>
+      );
+      const renderedIcon = wrapper.find(SystemIcon).first();
+
+      expect(wrapper.state().screenSize).toBe('lg');
+      expect(wrapper.find(SystemIcon)).toHaveLength(1);
+      expect(renderedIcon.props().color).toBe(expectedColors.color);
+      expect(renderedIcon.props().colorHover).toBe(expectedColors.colorHover);
+      expect(renderedIcon.props().icon).toBe(activityStreamIcon);
+    });
+
+    test('Renders a child hamburger menu (SystemIcon) when width is at "sm" breakpoint', () => {
+      window.resizeBy(319, 768);
+
+      const wrapper = mount<Header>(<Header>{icon}</Header>);
+      const renderedIcon = wrapper.find(SystemIcon).first();
+
+      expect(wrapper.state().screenSize).toBe('sm');
+      expect(renderedIcon.props().icon).toBe(justifyIcon);
+    });
   });
 });
