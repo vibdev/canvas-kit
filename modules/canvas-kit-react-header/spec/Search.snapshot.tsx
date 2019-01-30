9a5173b2486def5da44fcccaa41337a0de401767
Lynn Chyi <lynn.chyi@workday.com>
2019-01-29 17:05:52 -0800

    test(header): update search snapshots

--- modules/canvas-kit-react-header/spec/Search.snapshot.tsx
+++ modules/canvas-kit-react-header/spec/Search.snapshot.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import {mount} from 'enzyme';
 import * as renderer from 'react-test-renderer';
 import Header from '../lib/Header';
 import {Search} from '../lib/parts/Search';
@@ -10,11 +9,11 @@ describe('Header Search Snapshots', () => {
     cb.mockReset();
   });
 
-  // test('Renders a collapsed search bar', () => {
-  //   const component = mount(<Search collapse={true} />);
+  test('Renders a collapsed search bar', () => {
+    const component = renderer.create(<Search collapse={true} />);
 
-  //   expect(component).toMatchSnapshot();
-  // });
+    expect(component).toMatchSnapshot();
+  });
 
   test('renders themed search bars', () => {
     const tree = {
@@ -27,18 +26,16 @@ describe('Header Search Snapshots', () => {
     expect(tree.white).toMatchSnapshot('<Search /> with white theme applied');
   });
 
-  console.log('hi');
-
-  // test('renders themed, collapsed search bars', () => {
-  //   const tree = {
-  //     blue: renderer.create(<Search themeColor={Header.Theme.Blue} collapse={true} />).toJSON(),
-  //     transparent: renderer
-  //       .create(<Search themeColor={Header.Theme.Transparent} collapse={true} />)
-  //       .toJSON(),
-  //     white: renderer.create(<Search themeColor={Header.Theme.White} collapse={true} />).toJSON(),
-  //   };
-  //   expect(tree.blue).toMatchSnapshot('<Search /> with blue theme applied');
-  //   expect(tree.transparent).toMatchSnapshot('<Search /> with transparent theme applied');
-  //   expect(tree.white).toMatchSnapshot('<Search /> with white theme applied');
-  // });
+  test('renders themed, collapsed search bars', () => {
+    const tree = {
+      blue: renderer.create(<Search themeColor={Header.Theme.Blue} collapse={true} />).toJSON(),
+      transparent: renderer
+        .create(<Search themeColor={Header.Theme.Transparent} collapse={true} />)
+        .toJSON(),
+      white: renderer.create(<Search themeColor={Header.Theme.White} collapse={true} />).toJSON(),
+    };
+    expect(tree.blue).toMatchSnapshot('<Search /> with blue theme applied');
+    expect(tree.transparent).toMatchSnapshot('<Search /> with transparent theme applied');
+    expect(tree.white).toMatchSnapshot('<Search /> with white theme applied');
+  });
 });
