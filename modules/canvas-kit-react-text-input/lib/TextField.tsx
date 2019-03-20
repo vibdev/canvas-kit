ed146918dca80684c176e2ab79dd9968639a9022
Joseph Le <joseph.le@workday.com>
2019-03-20 16:05:55 -0700

    fix(text-input): make hint text optional

--- modules/canvas-kit-react-text-input/lib/TextField.tsx
+++ modules/canvas-kit-react-text-input/lib/TextField.tsx
@@ -8,7 +8,7 @@ import Hint from './Hint';
 
 export interface TextFieldProps extends TextInputProps {
   label: React.ReactNode;
-  hintText: React.ReactNode;
+  hintText?: React.ReactNode;
 }
 
 const InputContainer = styled('div')({
