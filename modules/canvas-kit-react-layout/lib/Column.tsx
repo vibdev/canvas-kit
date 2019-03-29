3cbacfbcea2881860abc8809259e2ff47623192b
chad sahlhoff <chad.sahlhoff@workday.com>
2019-03-29 16:07:13 -0700

    feat(layouts): limited columns to 1 through 12

--- modules/canvas-kit-react-layout/lib/Column.tsx
+++ modules/canvas-kit-react-layout/lib/Column.tsx
@@ -13,7 +13,7 @@ export interface ColumnProps {
   /**
    * Widths of columns.
    */
-  columns?: number;
+  columns?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   /**
    * Fixed width of columns
    */
