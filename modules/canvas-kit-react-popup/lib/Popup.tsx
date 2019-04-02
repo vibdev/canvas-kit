3ab567ababe81b6639ff804674574a0499b3d0e1
Alex Nicholls <anicholls3@gmail.com>
2019-04-02 10:27:59 -0700

    fix(popup): Tweak animation

--- modules/canvas-kit-react-popup/lib/Popup.tsx
+++ modules/canvas-kit-react-popup/lib/Popup.tsx
@@ -30,14 +30,15 @@ const closeIconSpacing = 12;
 
 const popupAnimation = keyframes`
 0% {
-  
-  transform: scale(0);
+  opacity: 0;
+  transform: scale(0.8);
 }
 100% {
-  
+  opacity: 1;
   transform: scale(1);
 }
 `;
+
 const Container = styled('div')<Pick<PopupProps, 'transformOrigin'>>(
   {
     position: 'relative',
