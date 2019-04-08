5331ff5d8780843b6d93f9c5b194503420927159
Joseph Le <joseph.le@workday.com>
2019-04-08 11:16:50 -0700

    docs(checkbox): add stories with FormField

--- modules/canvas-kit-react-checkbox/stories.tsx
+++ modules/canvas-kit-react-checkbox/stories.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import {InputProviderDecorator} from '@workday/canvas-kit-react-common';
+import FormField from '@workday/canvas-kit-react-form-field';
 
 import Checkbox from './index'; // tslint:disable-line:import-name
 import README from './README.md';
@@ -11,7 +12,6 @@ class CheckboxWrapper extends React.Component {
   state = {
     id: '1',
     isChecked: false,
-    label: 'This is a checkbox',
   };
 
   handleCheck = () => {
@@ -26,14 +26,14 @@ class CheckboxWrapper extends React.Component {
         disabled={false}
         checked={this.state.isChecked}
         id={this.state.id}
-        label={this.state.label}
+        label="Checkbox option"
         onChange={this.handleCheck}
       />
     );
   }
 }
 
-storiesOf('Canvas Kit/Checkbox', module)
+storiesOf('Canvas Kit/Input/Checkbox', module)
   .addDecorator(InputProviderDecorator)
   .addDecorator(withReadme(README))
   .add('Default', () => (
@@ -41,6 +41,30 @@ storiesOf('Canvas Kit/Checkbox', module)
       <h1 className="section-label">Checkbox</h1>
       <div style={{textAlign: 'left', marginBottom: '24px'}}>
         <CheckboxWrapper />
+      </div>
+    </div>
+  ));
+
+storiesOf('Canvas Kit/Input/Checkbox Field', module)
+  .addDecorator(InputProviderDecorator)
+  .addDecorator(withReadme(README))
+  .add('Top Label', () => (
+    <div className="story">
+      <h1 className="section-label">Checkbox</h1>
+      <div style={{textAlign: 'left', marginBottom: '24px'}}>
+        <FormField label="Checkbox Field">
+          <CheckboxWrapper />
+        </FormField>
+      </div>
+    </div>
+  ))
+  .add('Left Label', () => (
+    <div className="story">
+      <h1 className="section-label">Checkbox</h1>
+      <div style={{textAlign: 'left', marginBottom: '24px'}}>
+        <FormField label="Checkbox Field" labelPosition={FormField.LabelPosition.Left}>
+          <CheckboxWrapper />
+        </FormField>
       </div>
     </div>
   ));
