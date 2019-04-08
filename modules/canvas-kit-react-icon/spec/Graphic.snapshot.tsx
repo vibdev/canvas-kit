a152cf89b1815fd309de5d32e23eb2cb65f935cf
Justin Pante <justin.pante@workday.com>
2019-04-08 13:41:10 -0700

    test(graphics):  Use mock graphic rather than incompatible type

--- modules/canvas-kit-react-icon/spec/Graphic.snapshot.tsx
+++ modules/canvas-kit-react-icon/spec/Graphic.snapshot.tsx
@@ -1,11 +1,18 @@
 import * as renderer from 'react-test-renderer';
 import Graphic from '../lib/Graphic';
 import * as React from 'react';
-import {shieldIcon} from '@workday/canvas-accent-icons-web';
+import {CanvasGraphic, CanvasIconTypes} from '@workday/design-assets-types';
 
 describe('Graphic Snapshots', () => {
   test('renders as expected', () => {
-    const component = renderer.create(<Graphic src={shieldIcon} />);
+    const mockGraphic: CanvasGraphic = {
+      name: 'mockGraphic',
+      type: CanvasIconTypes.Graphic,
+      svg: '<svg></svg>',
+      filename: 'mock-graphic.svg',
+    };
+
+    const component = renderer.create(<Graphic src={mockGraphic} />);
     expect(component).toMatchSnapshot();
   });
 });
