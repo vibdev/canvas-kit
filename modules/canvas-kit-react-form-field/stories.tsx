7844418840864ae6f3f9295587a38fa9a1820225
Joseph Le <joseph.le@workday.com>
2019-04-08 16:43:00 -0700

    docs(form-field): consolidate text field stories

--- modules/canvas-kit-react-form-field/stories.tsx
+++ modules/canvas-kit-react-form-field/stories.tsx
@@ -2,11 +2,13 @@
 import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
+import {SectionDecorator} from '@workday/canvas-kit-react-common';
 
-import {Label, Hint} from './index'; // tslint:disable-line:import-name
+import FormField, {FormFieldProps, Label, Hint} from './index'; // tslint:disable-line:import-name
 import README from './README.md';
+import {Input as TextInput, Inputs} from '../canvas-kit-react-text-input/stories';
 
-storiesOf('Canvas Kit/Input/Base', module)
+storiesOf('Canvas Kit/Input', module)
   .addDecorator(withReadme(README))
   .add('Label', () => (
     <div className="story">
@@ -21,4 +23,71 @@ storiesOf('Canvas Kit/Input/Base', module)
       <Hint error={Hint.ErrorType.Error}>Hint</Hint>
       <Hint error={Hint.ErrorType.Alert}>Hint</Hint>
     </div>
+  ));
+
+class Field extends React.Component<FormFieldProps> {
+  static defaultProps = {
+    labelPosition: FormField.LabelPosition.Top,
+  };
+
+  render() {
+    return (
+      <FormField label="Label" {...this.props}>
+        {this.props.children}
+      </FormField>
+    );
+  }
+}
+
+const hintText = 'Helpful text goes here.';
+
+storiesOf('Canvas Kit/Form Field/Text/Top Label', module)
+  .addDecorator(SectionDecorator('Text Field'))
+  .addDecorator(withReadme(README))
+  .add('Plain', () => <Field>{Inputs.Plain}</Field>)
+  .add('With placeholder', () => <Field>{Inputs.Placeholder}</Field>)
+  .add('Disabled', () => <Field>{Inputs.Disabled}</Field>)
+  .add('Disabled with placeholder', () => <Field>{Inputs.DisabledPlaceholder}</Field>)
+  .add('Alert', () => <Field>{Inputs.Alert}</Field>)
+  .add('Error', () => <Field>{Inputs.Error}</Field>)
+  .add('Grow', () => (
+    <Field grow={true}>
+      <TextInput {...Inputs.Placeholder.props} />
+    </Field>
+  ))
+  .add('Grow - Error', () => (
+    <Field grow={true} error={FormField.ErrorType.Error} hintText={hintText}>
+      <TextInput {...Inputs.Placeholder.props} />
+    </Field>
+  ));
+
+storiesOf('Canvas Kit/Form Field/Text/Left Label', module)
+  .addDecorator(SectionDecorator('Text Field'))
+  .addDecorator(withReadme(README))
+  .add('Plain', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Plain}</Field>)
+  .add('With placeholder', () => (
+    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Placeholder}</Field>
+  ))
+  .add('Disabled', () => (
+    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Disabled}</Field>
+  ))
+  .add('Disabled with placeholder', () => (
+    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.DisabledPlaceholder}</Field>
+  ))
+  .add('Alert', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Alert}</Field>)
+  .add('Error', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Error}</Field>)
+  .add('Grow', () => (
+    <Field labelPosition={FormField.LabelPosition.Left} grow={true}>
+      <TextInput {...Inputs.Placeholder.props} />
+    </Field>
+  ))
+  .add('Grow - Error', () => (
+    <Field
+      labelPosition={FormField.LabelPosition.Left}
+      grow={true}
+      error={FormField.ErrorType.Error}
+      hintText={hintText}
+    >
+      <TextInput {...Inputs.Placeholder.props} />
+    </Field>
   ));
--- modules/canvas-kit-react-text-input/stories.tsx
+++ modules/canvas-kit-react-text-input/stories.tsx
@@ -3,12 +3,11 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import {InputProviderDecorator, SectionDecorator} from '@workday/canvas-kit-react-common';
-import FormField, {FormFieldProps} from '@workday/canvas-kit-react-form-field';
 
 import {TextInput, TextInputProps} from './index'; // tslint:disable-line:import-name
 import README from './README.md';
 
-class Input extends React.Component<TextInputProps, {}> {
+export class Input extends React.Component<TextInputProps, {}> {
   static defaultProps = {
     type: 'text',
   };
@@ -27,23 +26,7 @@ class Input extends React.Component<TextInputProps, {}> {
   }
 }
 
-class Field extends React.Component<FormFieldProps> {
-  static defaultProps = {
-    labelPosition: FormField.LabelPosition.Top,
-  };
-
-  render() {
-    return (
-      <FormField label="Label" {...this.props}>
-        {this.props.children}
-      </FormField>
-    );
-  }
-}
-
-const hintText = 'Helpful text goes here.';
-
-const Inputs = {
+export const Inputs = {
   Plain: <Input />,
   Placeholder: <Input placeholder="Placeholder" />,
   Disabled: <Input disabled={true} />,
@@ -66,54 +49,3 @@ storiesOf('Canvas Kit/Input/Base/Text Input', module)
   .add('Error', () => Inputs.Error)
   .add('Grow', () => Inputs.Grow)
   .add('Grow - Error', () => Inputs.GrowError);
-
-storiesOf('Canvas Kit/Input/Text Field/Top Label', module)
-  .addDecorator(SectionDecorator('Text Field'))
-  .addDecorator(withReadme(README))
-  .add('Plain', () => <Field>{Inputs.Plain}</Field>)
-  .add('With placeholder', () => <Field>{Inputs.Placeholder}</Field>)
-  .add('Disabled', () => <Field>{Inputs.Disabled}</Field>)
-  .add('Disabled with placeholder', () => <Field>{Inputs.DisabledPlaceholder}</Field>)
-  .add('Alert', () => <Field>{Inputs.Alert}</Field>)
-  .add('Error', () => <Field>{Inputs.Error}</Field>)
-  .add('Grow', () => (
-    <Field grow={true}>
-      <Input {...Inputs.Placeholder.props} />
-    </Field>
-  ))
-  .add('Grow - Error', () => (
-    <Field grow={true} error={FormField.ErrorType.Error} hintText={hintText}>
-      <Input {...Inputs.Placeholder.props} />
-    </Field>
-  ));
-
-storiesOf('Canvas Kit/Input/Text Field/Left Label', module)
-  .addDecorator(SectionDecorator('Text Field'))
-  .addDecorator(withReadme(README))
-  .add('Plain', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Plain}</Field>)
-  .add('With placeholder', () => (
-    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Placeholder}</Field>
-  ))
-  .add('Disabled', () => (
-    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Disabled}</Field>
-  ))
-  .add('Disabled with placeholder', () => (
-    <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.DisabledPlaceholder}</Field>
-  ))
-  .add('Alert', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Alert}</Field>)
-  .add('Error', () => <Field labelPosition={FormField.LabelPosition.Left}>{Inputs.Error}</Field>)
-  .add('Grow', () => (
-    <Field labelPosition={FormField.LabelPosition.Left} grow={true}>
-      <Input {...Inputs.Placeholder.props} />
-    </Field>
-  ))
-  .add('Grow - Error', () => (
-    <Field
-      labelPosition={FormField.LabelPosition.Left}
-      grow={true}
-      error={FormField.ErrorType.Error}
-      hintText={hintText}
-    >
-      <Input {...Inputs.Placeholder.props} />
-    </Field>
-  ));
