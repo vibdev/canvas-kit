76e31721c23b7603b04c72497ca713e96871db12
Alex Nicholls <anicholls3@gmail.com>
2019-04-08 17:13:53 -0700

    feat(button): Export missing props from button

--- modules/canvas-kit-react-button/index.tsx
+++ modules/canvas-kit-react-button/index.tsx
@@ -2,8 +2,11 @@ import Button from './lib/Button';
 
 export default Button;
 export {default as Button, beta_Button, ButtonProps} from './lib/Button';
-export {default as IconButton} from './lib/IconButton';
+export {default as IconButton, IconButtonProps} from './lib/IconButton';
 export {default as DropdownButton} from './lib/DropdownButton';
-export {default as TextButton} from './lib/TextButton';
-export {default as IconButtonToggleGroup} from './lib/IconButtonToggleGroup';
+export {default as TextButton, TextButtonProps} from './lib/TextButton';
+export {
+  default as IconButtonToggleGroup,
+  IconButtonToggleGroupProps,
+} from './lib/IconButtonToggleGroup';
 export * from './lib/types';
