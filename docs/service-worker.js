eeb3f0da57dcadb5dbc0b7a45b3d6bcb1b89e9ae
design-build <design-build@workday.com>
2019-06-21 18:56:45 +0000

    docs(storybook): Build Storybook [ci skip]

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e96a293fe9851eb40c93a591bb2bb841.js"
+  "precache-manifest.b95d7ab61a2681ccac3741549ecbc3c4.js"
 );
 
 self.addEventListener('message', (event) => {
