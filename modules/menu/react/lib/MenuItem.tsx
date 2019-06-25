f8879d9efcc86c6cffff0f329898cd7a9e34a624
Matthew Rust <matthew.rust@workday.com>
2019-06-25 13:06:02 -0700

    fix(menu): Update props to allow for standard html attributes
    Note this removes the label prop in favor of passing in aria-label directly when needed
    This also provides an example of how to use menu items in a combobox

--- modules/menu/react/lib/MenuItem.tsx
+++ modules/menu/react/lib/MenuItem.tsx
@@ -11,18 +11,16 @@ import {
 import {CanvasSystemIcon} from '@workday/design-assets-types';
 import {SystemIcon, SystemIconProps} from '@workday/canvas-kit-react-icon';
 
-export interface MenuItemProps {
+export interface MenuItemProps extends React.HTMLAttributes<HTMLLIElement> {
   onClick?: (event: React.SyntheticEvent) => void;
   children?: React.ReactNode;
   id?: string;
   icon?: CanvasSystemIcon;
   secondaryIcon?: CanvasSystemIcon;
-  label?: string;
   hasDivider?: boolean;
   isDisabled?: boolean;
   isFocused?: boolean;
-  role?: string;
-  useAriaSelected?: boolean;
+  role: string;
 }
 
 const Item = styled('li')<Pick<MenuItemProps, 'isDisabled' | 'isFocused'>>(
@@ -52,7 +50,7 @@ const Item = styled('li')<Pick<MenuItemProps, 'isDisabled' | 'isFocused'>>(
             fill: iconColors.hover,
           },
         },
-      }
+      };
     } else if (isDisabled && !isFocused) {
       return {
         color: colors.licorice100,
@@ -61,7 +59,8 @@ const Item = styled('li')<Pick<MenuItemProps, 'isDisabled' | 'isFocused'>>(
           fill: iconColors.disabled,
         },
       };
-    } else { // Is focused or focused and disabled
+    } else {
+      // Is focused or focused and disabled
       return {
         backgroundColor: isDisabled ? colors.blueberry200 : commonColors.focusBackground,
         color: typeColors.inverse,
@@ -153,14 +152,14 @@ export default class MenuItem extends React.Component<MenuItemProps> {
     const {
       onClick,
       children,
+      id,
       icon,
       secondaryIcon,
+      hasDivider,
       isDisabled,
       isFocused,
-      label,
-      hasDivider,
-      useAriaSelected,
-      ...otherProps
+      role,
+      ...elemProps
     } = this.props;
 
     iconProps = setIconProps(icon, isDisabled, isFocused);
@@ -171,17 +170,21 @@ export default class MenuItem extends React.Component<MenuItemProps> {
         {hasDivider && <Divider />}
         <Item
           tabIndex={-1}
-          aria-label={label}
+          id={id}
+          role={role}
           onClick={event => this.handleClick(event)}
           aria-disabled={!!isDisabled}
           isDisabled={!!isDisabled}
           isFocused={!!isFocused}
-          aria-selected={useAriaSelected && !!isFocused}
-          {...otherProps}
+          {...elemProps}
         >
-          {icon && iconProps && <StyledSystemIcon {...iconProps} />}
+          {icon && iconProps && (
+            <StyledSystemIcon {...iconProps} />
+          )}
           <LabelContainer>{children}</LabelContainer>
-          {secondaryIcon && secondaryIconProps && <SecondaryStyledSystemIcon {...secondaryIconProps} />}
+          {secondaryIcon && secondaryIconProps && (
+            <SecondaryStyledSystemIcon {...secondaryIconProps} />
+          )}
         </Item>
       </>
     );
